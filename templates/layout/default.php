262fdd84ece77f3b46f1b20a54a00d8c2d5ad874
saii0001 <saii0001@student.monash.edu>
2022-04-08 22:38:58 +1000

    Update default.php with css

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -21,16 +21,21 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
 <html>
 <head>
     <?= $this->Html->charset() ?>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-        <?= $cakeDescription ?>:
-        <?= $this->fetch('title') ?>
-    </title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <?= $this->Html->meta('icon') ?>
+
+    <!-- Custom fonts for this template-->
+    <?= $this->Html->css('/vendor/fontawesome-free/css/all.min.css') ?>
+    <title><?= $this->fetch('title') ?></title>
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
+    <!-- Custom styles for this template-->
+    <?= $this->Html->css('sb-admin-2.min.css') ?>
+    <!--  $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>-->
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
@@ -38,25 +43,86 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
 
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
-            <?= $this->Flash->render() ?>
-            <?= $this->fetch('content') ?>
-        </div>
-    </main>
-    <footer>
-        <?= $this->fetch('script') ?>
-    </footer>
+<div id="content">
+    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
+    <!-- Sidebar Toggle (Topbar) -->
+    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+        <i class="fa fa-bars"></i>
+    </button>
+
+    <!-- Topbar Navbar -->
+
+    <ul class="navbar-nav ml-auto">
+        <!-- Homepage -->
+        <li class="nav-item">
+            <a class="nav-link " <?= $this->Html->link(__('Homepage'), ['controller' => 'Pages','action' => 'home']) ?>
+            </a>
+
+        </li>
+
+        <!-- Customers -->
+        <li class="nav-item">
+            <a class="nav-link " <?= $this->Html->link(__('Customers'), ['controller' => 'Customers','action' => 'index']) ?>
+            </a>
+
+        </li>
+
+        <!-- Products -->
+        <li class="nav-item">
+            <a class="nav-link "<?= $this->Html->link(__('Products'), ['controller' => 'Products','action' => 'index']) ?>
+            </a>
+        </li>
+
+        <!-- Suppliers -->
+        <li class="nav-item">
+            <a class="nav-link "<?= $this->Html->link(__('Suppliers'), ['controller' => 'Suppliers','action' => 'index']) ?>
+            </a>
+        </li>
+
+        <!-- Orders -->
+        <li class="nav-item">
+            <a class="nav-link " <?= $this->Html->link(__('Orders'), ['controller' => 'RestockingOrders','action' => 'index']) ?>
+            </a>
+        </li>
+
+
+        <!-- Nav Item - User Information -->
+        <li class="nav-item dropdown no-arrow">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
+                <img class="img-profile rounded-circle"
+                     src="img/undraw_profile.svg">
+            </a>
+            <!-- Dropdown - User Information -->
+            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                 aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="#">
+                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Profile
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Logout
+                </a>
+            </div>
+        </li>
+
+    </ul>
+
+</nav>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    <?= $this->Flash->render() ?>
+    <?= $this->fetch('content') ?>
+</div>
+<!-- /.container-fluid -->
+</div>
+<footer>
+    <?= $this->fetch('script') ?>
+</footer>
 
 
 </body>
