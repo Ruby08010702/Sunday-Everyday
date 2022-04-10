841a4c61c502fb9bc4bb48447e5153ace7bf518f
saii0001 <saii0001@student.monash.edu>
2022-04-10 13:49:36 +1000

    view

--- templates/Products/view.php
+++ templates/Products/view.php
@@ -47,3 +47,63 @@
         </div>
     </div>
 </div>
+<div class="related">
+    <h4><?= __('Related Product Orders') ?></h4>
+    <?php if (!empty($product->product_orders)) : ?>
+        <div class="table-responsive">
+            <table>
+                <tr>
+                    <th><?= __('Id') ?></th>
+                    <th><?= __('Amount') ?></th>
+                    <th><?= __('Product Id') ?></th>
+                    <th><?= __('Order Id') ?></th>
+                    <th class="actions"><?= __('Actions') ?></th>
+                </tr>
+                <?php foreach ($product->product_orders as $productOrders) : ?>
+                    <tr>
+                        <td><?= h($productOrders->id) ?></td>
+                        <td><?= h($productOrders->amount) ?></td>
+                        <td><?= h($productOrders->product_id) ?></td>
+                        <td><?= h($productOrders->order_id) ?></td>
+                        <td class="actions">
+                            <?= $this->Html->link(__('View'), ['controller' => 'ProductOrders', 'action' => 'view', $productOrders->id]) ?>
+                            <?= $this->Html->link(__('Edit'), ['controller' => 'ProductOrders', 'action' => 'edit', $productOrders->id]) ?>
+                            <?= $this->Form->postLink(__('Delete'), ['controller' => 'ProductOrders', 'action' => 'delete', $productOrders->id], ['confirm' => __('Are you sure you want to delete # {0}?', $productOrders->id)]) ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+    <?php endif; ?>
+</div>
+<div class="related">
+    <h4><?= __('Related Product Restock') ?></h4>
+    <?php if (!empty($product->product_restock)) : ?>
+        <div class="table-responsive">
+            <table>
+                <tr>
+                    <th><?= __('Id') ?></th>
+                    <th><?= __('Amount') ?></th>
+                    <th><?= __('Cost') ?></th>
+                    <th><?= __('Product Id') ?></th>
+                    <th><?= __('Rp Id') ?></th>
+                    <th class="actions"><?= __('Actions') ?></th>
+                </tr>
+                <?php foreach ($product->product_restock as $productRestock) : ?>
+                    <tr>
+                        <td><?= h($productRestock->id) ?></td>
+                        <td><?= h($productRestock->amount) ?></td>
+                        <td><?= h($productRestock->cost) ?></td>
+                        <td><?= h($productRestock->product_id) ?></td>
+                        <td><?= h($productRestock->rp_id) ?></td>
+                        <td class="actions">
+                            <?= $this->Html->link(__('View'), ['controller' => 'ProductRestock', 'action' => 'view', $productRestock->id]) ?>
+                            <?= $this->Html->link(__('Edit'), ['controller' => 'ProductRestock', 'action' => 'edit', $productRestock->id]) ?>
+                            <?= $this->Form->postLink(__('Delete'), ['controller' => 'ProductRestock', 'action' => 'delete', $productRestock->id], ['confirm' => __('Are you sure you want to delete # {0}?', $productRestock->id)]) ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+    <?php endif; ?>
+</div>
