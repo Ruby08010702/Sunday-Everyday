ff06d7ef5ed7f4f6a6639314a80e9b46623c2ba0
njbag1 <njbag1@student.monash.edu>
2022-04-10 20:58:46 +1000

    NB - Updated CSS - 10/04/2022
    - home.css  = homepage/dashbaord
    - sb.admin-2.min.css = table pages
    - default.php = added image to navbar

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -97,9 +97,9 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small"></span>
                 <img class="img-profile rounded-circle"
-                     src="img/undraw_profile.svg">
+                     src="img/sundayEveryday_logo_blue.png" alt="">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
