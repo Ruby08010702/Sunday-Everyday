bd214b2a3a297334b783b8c8c5dc5f84db93f686
yzha0631 <yzha0631@student.monash.edu>
2022-04-27 21:08:04 +0800

    staff update

--- templates/Staffs/edit.php
+++ templates/Staffs/edit.php
@@ -7,11 +7,11 @@
 <h1 class="h3 mb-2 text-gray-800">Edit Staff</h1>
 <?= $this->Form->create($staff) ?>
 <?php
-echo $this->Form->control('first_name',['pattern'=>'^[A-Za-z ]+{1,24}']);
-echo $this->Form->control('last_name',['pattern'=>'^[A-Za-z ]+{1,24}']);
+echo $this->Form->control('first_name',['pattern'=>'^[A-Za-z ]+$']);
+echo $this->Form->control('last_name',['pattern'=>'^[A-Za-z ]+$']);
 echo $this->Form->control('address', ['pattern' => '^[A-Za-z0-9, ]{3,40}']);
-echo $this->Form->control('email', ['pattern' => '^{7,20}']);
-echo $this->Form->control('phone', ['pattern' => '^[0-9]{10,10}']);
+echo $this->Form->control('email',['pattern'=>'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$']);
+echo $this->Form->control('phone',['pattern' => '^[0-9]{10,10}'], ['placeholder'=>'e.g. 0412345678']);
 ?>
 <?= $this->Form->button(__('Submit'),['class'=>'btn btn-primary']) ?>
 <?= $this->Form->end() ?>
