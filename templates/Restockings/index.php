dc3ae80fd0fd3d46b1d8febc4ddf6fe8b3187707
efre0006 <efre0006@student.monash.edu>
2022-04-27 21:13:30 +1000

    restocking page fix

--- templates/Restockings/index.php
+++ templates/Restockings/index.php
@@ -3,48 +3,58 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Restocking[]|\Cake\Collection\CollectionInterface $restockings
  */
+
+echo $this->Html->css('/vendor/datatables/dataTables.bootstrap4.min.css',['block'=>true]);
+echo $this->Html->script('/vendor/datatables/jquery.dataTables.min.js',['block'=>true]);
+echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js',['block'=>true]);
 ?>
-<div class="restockings index content">
-    <?= $this->Html->link(__('New Restocking'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Restockings') ?></h3>
+
+
+<div class="customers index content">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"><?= __('Restockings') ?></h1>
+        <a href="<?= $this->Url->build(['action'=>'add']) ?>" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-user-plus fa-sm text-white-50"></i> New Restocking</a>
+    </div>
     <div class="table-responsive">
-        <table>
+        <table  class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
             <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('staff_id') ?></th>
-                    <th><?= $this->Paginator->sort('date') ?></th>
-                    <th><?= $this->Paginator->sort('payment') ?></th>
-                    <th><?= $this->Paginator->sort('quantity') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
+            <tr>
+
+                <th><?= h('id') ?></th>
+                <th><?= h('staff_id') ?></th>
+                <th><?= h('date') ?></th>
+                <th><?= h('payment') ?></th>
+                <th><?= h('quantity') ?></th>
+
+                <th class="actions"><?= __('Actions') ?></th>
+            </tr>
             </thead>
             <tbody>
-                <?php foreach ($restockings as $restocking): ?>
+            <?php foreach ($restockings as $restocking): ?>
                 <tr>
                     <td><?= h($restocking->id) ?></td>
-                    <td><?= $restocking->has('staff') ? $this->Html->link($restocking->staff->id, ['controller' => 'Staffs', 'action' => 'view', $restocking->staff->id]) : '' ?></td>
+                    <td><?= $restocking->has('staff') ? $this->Html->link($restocking->staff->first_name, ['controller' => 'Staffs', 'action' => 'view', $restocking->staff->id]) : '' ?></td>
                     <td><?= h($restocking->date) ?></td>
                     <td><?= h($restocking->payment) ?></td>
-                    <td><?= $this->Number->format($restocking->quantity) ?></td>
+                    <td><?= h($restocking->quantity) ?></td>
                     <td class="actions">
                         <?= $this->Html->link(__('View'), ['action' => 'view', $restocking->id]) ?>
                         <?= $this->Html->link(__('Edit'), ['action' => 'edit', $restocking->id]) ?>
                         <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $restocking->id], ['confirm' => __('Are you sure you want to delete # {0}?', $restocking->id)]) ?>
                     </td>
                 </tr>
-                <?php endforeach; ?>
+            <?php endforeach; ?>
             </tbody>
         </table>
     </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-    </div>
+
+    <script>
+        $(document).ready(function (){
+            $('#dataTable').DataTable();
+        });
+    </script>
 </div>
+
+
+<!--------------->
