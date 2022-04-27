7d09ec05a17f79af525c85befffc39709642de1e
efre0006 <efre0006@student.monash.edu>
2022-04-27 20:46:53 +1000

    validation

--- templates/Products/add.php
+++ templates/Products/add.php
@@ -9,10 +9,10 @@
 <h1 class="h3 mb-2 text-gray-800">Add new Product</h1>
 <?= $this->Form->create($product) ?>
 <?php
-echo $this->Form->control('name',['pattern'=>'^[A-Za-z]+$']);
-echo $this->Form->control('cost');
-echo $this->Form->control('retail_price');
-echo $this->Form->control('quantity',['max'=>999]);
+echo $this->Form->control('name',['pattern' => '^[A-Za-z0-9 ]{0-24}']);
+echo $this->Form->control('cost',['pattern' => '^[0-9]{0-10}'] );
+echo $this->Form->control('retail_price',['pattern' => '^[0-9]{0-10}'] );
+echo $this->Form->control('quantity',['max'=>999999]);
 echo $this->Form->control('supplier_id', ['options' => $suppliers]);
 ?>
 <?= $this->Form->button(__('Submit'),['class'=>'btn btn-primary']) ?>
