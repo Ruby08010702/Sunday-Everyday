abee78c692662d4eab33ee3243ac38ce8c9ae80f
saii0001 <saii0001@student.monash.edu>
2022-05-04 18:39:03 +1000

    some login

--- src/Model/Entity/Staff.php
+++ src/Model/Entity/Staff.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 namespace App\Model\Entity;
-
+use Authentication\PasswordHasher\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -38,4 +38,11 @@ class Staff extends Entity
         'phone' => true,
         'restockings' => true,
     ];
+
+    protected function _setPassword(string $password) : ?string
+    {
+        if (strlen($password) > 0) {
+            return (new DefaultPasswordHasher())->hash($password);
+        }
+    }
 }
