5c9fd4bb4c00c5dd36e8872e31ed3172ef707fd8
njbag1 <njbag1@student.monash.edu>
2022-05-12 10:10:21 +1000

    NB - Reformat Update (12/05/2022)
    Changes:
    - Profile Link removed
    - Random < removed
    
    To Do:
    - Understock table
    - Cart
    - Guest view page

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -1,4 +1,4 @@
-<<?php
+<?php
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Product[]|\Cake\Collection\CollectionInterface $products
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 
     <?= $this->Html->charset() ?>
@@ -214,15 +213,18 @@
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
                             <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $this->Identity->get('username')?></span>
                             <?=$this->Html->image('undraw_profile.svg',['class'=>'img-profile rounded-circle']);?>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                              aria-labelledby="userDropdown">
+                            <!--
                             <a class="dropdown-item" href="#">
                                 <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                 Profile
                             </a>
+                            -->
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
