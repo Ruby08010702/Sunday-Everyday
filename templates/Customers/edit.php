c11c3a5dad7cd0d2c37f504567f2c92bf590182c
saii0001 <saii0001@student.monash.edu>
2022-05-17 21:45:06 +1000

    Update: customer names back to customer list

--- templates/Customers/edit.php
+++ templates/Customers/edit.php
@@ -10,6 +10,7 @@
 <?php
 echo $this->Form->control('first_name',['pattern'=>'^[A-Za-z ]+${1-64',  'style' => 'width:300px;']);
 echo $this->Form->control('last_name',['pattern'=>'^[A-Za-z ]+${1-64}',  'style' => 'width:300px;']);
+echo $this->Form->control('username',['pattern'=>'^[A-Za-z ]+${1-64}',  'style' => 'width:300px;']);
 echo $this->Form->control('address', ['pattern' => '^[A-Za-z0-9, ]{3,100}',  'style' => 'width:300px;']);
 echo $this->Form->control('email',['pattern'=>'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*${5-64}',  'style' => 'width:300px;']);
 echo $this->Form->control('phone',['pattern' => '^[0-9]{10,10}',  'style' => 'width:300px;'], ['placeholder'=>'e.g. 0412345678']);
