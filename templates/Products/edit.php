5484cb83cfab232de7fbac6e838fd5b11b999a0e
saii0001 <saii0001@student.monash.edu>
2022-05-17 20:30:14 +1000

    Update: add edit in product page

--- templates/Products/edit.php
+++ templates/Products/edit.php
@@ -7,9 +7,10 @@
 ?>
 
 <h1 class="h3 mb-2 text-gray-800">Edit Existing Product</h1>
-<?= $this->Form->create($product) ?>
+<?= $this->Form->create($product,['type'=>'file']) ?>
 <?php
 echo $this->Form->control('name',['pattern' => '^[A-Za-z0-9 ]{0-24}',  'style' => 'width:300px;']);
+echo $this->Form->control('image_file',['type'=>'file' ]);
 echo $this->Form->control('cost',['max'=>"9999.99", 'min' => "0.00",'step'=>"0.01",  'style' => 'width:300px;'],['pattern' => '^[0-9]{0-10}'] );
 echo $this->Form->control('retail_price',['max'=>"9999.99", 'min' => "0.00",'step'=>"0.01",  'style' => 'width:300px;'],['pattern' => '^[0-9]{0-10}'] );
 echo $this->Form->control('quantity',['max'=>99999, 'default' => 0,  'style' => 'width:300px;']);
