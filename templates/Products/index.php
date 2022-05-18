51ed0c069ad7c5de8bfc1843c1282dc807e79b25
saii0001 <saii0001@student.monash.edu>
2022-05-18 22:09:47 +1000

    Update: update product

--- templates/Products/index.php
+++ templates/Products/index.php
@@ -79,11 +79,13 @@ echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js',['blo
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h4 class="modal-title" id="exampleModalLongTitle"><?= h($product->name) ?></h4>
+                                    <button type="button" class="fa fa-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                </div>
+                                <div class="modal-text">
                                     <h5 class="modal-text" >Cost: $<?= h($product->cost) ?></h5>
                                     <h5 class="modal-text" >Retail Price: $<?= h($product->retail_price) ?></h5>
                                     <h5 class="modal-text" >Supplier: <?= h($product->supplier->business_name) ?></h5>
-                                    <button type="button" class="fa fa-close" data-bs-dismiss="modal"
-                                            aria-label="Close"></button>
                                 </div>
                                 <div class="modal-body">
                                     <div class="row align-items-center">
